45f9b7e4bbd0c44dbef9315e800a89003fd8f035
Chuiwen Ma <machuiwen@gmail.com>
2017-02-22 00:14:30 -0800

    Report error when multiple python files are found and no one is main.py

--- buildserver.py
+++ buildserver.py
@@ -7,6 +7,7 @@ import re
 
 ROOT_DIR = os.path.dirname(os.path.realpath(__file__))
 LOG = []
+DEFAULT_PYCODE = 'main.py'
 with open('conf/my_email', 'r') as f:
     MY_EMAIL = f.readline().strip()
 with open('conf/whitelist_emails', 'r') as f:
@@ -36,6 +37,38 @@ def get_from_emails_query(emails):
     query += '}'
     return query
 
+def build(store_dir, test_id):
+    # attach source code
+    os.chdir(store_dir)
+    code_files = get_cmd_output('find %s -name *.py' % store_dir).split('\n')
+    pycode = None
+    if len(code_files) == 1:
+        pycode = code_files[0]
+    elif len(code_files) > 1:
+        if os.path.join(store_dir, DEFAULT_PYCODE) in code_files:
+            pycode = os.path.join(store_dir, DEFAULT_PYCODE)
+        else:
+            log("Error: Multiple python files found. Please set one as main.py.")
+
+    if pycode:
+        log("#################### TEST ID ###################")
+        log("Test ID: %s" % test_id)
+        log("#################### SRC CODE ##################")
+        log(get_cmd_output('cat %s' % pycode))
+
+        # run python code
+        start_time = time.time()
+        out, err = run_command('python %s' % pycode)
+        total_time = time.time() - start_time
+        log("#################### OUTPUT ####################")
+        log(out)
+        log("#################### ERROR #####################")
+        log(err)
+        log("#################### TOTAL TIME ################")
+        log("Total time: %.3f seconds" % total_time)
+        log("################################################")
+
+
 def main():
     client = GmailClient()
 
@@ -63,30 +96,15 @@ def main():
         client.GetAttachments('me', msg['id'], store_dir)
         attachments = set(os.listdir(store_dir))
 
-        # attach source code
-        os.chdir(store_dir)
-        pycode = get_cmd_output('find %s -name *.py' % store_dir)
-        log("#################### SRC CODE ##################")
-        log(get_cmd_output('cat %s' % pycode))
-
-        # run python code
-        start_time = time.time()
-        out, err = run_command('python %s' % pycode)
-        total_time = time.time() - start_time
-        log("#################### OUTPUT ####################")
-        log(out)
-        log("#################### ERROR #####################")
-        log(err)
-        log("#################### TOTAL TIME ################")
-        log("Total time: %.3f seconds" % total_time)
-        log("################################################")
+        # run build job
+        build(store_dir, test_id)
 
         # check generated files
         generated_files = set(os.listdir(store_dir)) - attachments
         generated_files = [os.path.join(store_dir, f) for f in generated_files]
 
         # send email
-        subject = "Result for running %s, Test ID: %s" % (pycode.split('/')[-1], test_id)
+        subject = "Build result"
         content = '\n'.join(LOG) + '\n'
         if generated_files:
             message = GmailClient.CreateMessageWithAttachment(MY_EMAIL, from_email, subject,
